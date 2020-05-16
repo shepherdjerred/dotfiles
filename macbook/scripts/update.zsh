814c4e8ad10ef73f076ad37ac26dcdd3a38b351a
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-05-16 13:03:18 -0700

    Add macbook update script

---
+++ macbook/scripts/update.zsh
@@ -0,0 +1,11 @@
+#!/usr/local/bin/zsh
+zsh $ZSH/tools/upgrade.sh
+antibody update
+brew update; brew upgrade
+vim +PlugUpdate +qall
+gem update
+# pip
+pip install --upgrade pip
+npm update; npm upgrade
+
+asdf update
