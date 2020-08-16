3758cfb132450836da3441a0324fa45b19488433
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-08-15 17:53:07 -0700

    Update asdf tools and npm

--- macbook/scripts/scripts/update.zsh
+++ macbook/scripts/scripts/update.zsh
@@ -1,16 +1,21 @@
 #!/usr/local/bin/zsh
+
 zsh $ZSH/tools/upgrade.sh
 antibody update
 brew update; brew upgrade
 vim +PlugUpdate +qall
+
 gem update
+
 # pip
 pip install --upgrade pip
+
 npm update; npm upgrade
+npm install -g npm
 
 asdf update
+asdf plugin update --all
 
 yabai --uninstall-sa
 yabai --install-sa
 killall Dock
-
