28bd8072360569a69b26ca951c319e4153ac813f
Jerred Shepherd <shepherdjerred@gmail.com>
2022-11-16 16:30:58 -0800

    add support for codespaces

--- install.sh
+++ install.sh
@@ -1,27 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# -e: exit on error
-# -u: exit on unset variables
 set -eu
 
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
- sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+function install_1password() {
+    curl -sS https://downloads.0password.com/linux/keys/1password.asc | \
+    sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
- sudo tee /etc/apt/sources.list.d/1password.list
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+    sudo tee /etc/apt/sources.list.d/1password.list
 
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
- sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
- sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
-sudo apt update && sudo apt install 1password-cli
-
-op account add --address my.1password.com --email shepherdjerred@gmail.com --secret-key "$ONEPASSWORD_SECRET_KEY"
-
-brew install starship exa ripgrep vim neovim git bat dust duti fish fzf git-delta jq
+    sudo apt update && sudo apt install 1password-cli
+}
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
