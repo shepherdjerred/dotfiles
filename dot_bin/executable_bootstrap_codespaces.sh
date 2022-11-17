ce5f8716b534f55b5872e70193edbe7a35f9ebce
Jerred Shepherd <shepherdjerred@gmail.com>
2022-11-17 11:14:27 -0800

    Add non-interactive and interactive scripts

--- dot_bin/executable_bootstrap_codespaces.sh
+++ dot_bin/executable_bootstrap_codespaces.sh
@@ -2,8 +2,45 @@
 
 set -eu
 
+function install_1password() {
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+        sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+        sudo tee /etc/apt/sources.list.d/1password.list
+
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
+        sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+        sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+    sudo apt update && sudo apt install 1password-cli
+}
+
+function setup_fish() {
+    fish -c "fisher update"
+    sudo add-shell "$(which fish)"
+    sudo chsh --shell /home/linuxbrew/.linuxbrew/bin/fish "$USER"
+}
+
+function setup_asdf() {
+    asdf plugin add python
+    asdf plugin add nodejs
+    asdf plugin add rust
+    asdf install
+}
+
+install_1password
 op account add --address my.1password.com --email shepherdjerred@gmail.com --secret-key "$ONEPASSWORD_SECRET_KEY"
+chezmoi apply --source="."
+
+brew bundle install --file dot_homebrew/codespaces.Brewfile
+
+setup_asdf
+setup_fish
+
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 gpg --import "$HOME"/.gnupg/public.asc
 gpg --import "$HOME"/.gnupg/secret.asc
-chezmoi init --apply --source="."
--- install.sh
+++ install.sh
@@ -2,37 +2,12 @@
 
 set -eu
 
-function install_1password() {
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-        sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+brew install chezmoi
 
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-        sudo tee /etc/apt/sources.list.d/1password.list
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
-        sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-        sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+set -- init --apply --source="${script_dir}" --force --promptString system_package_manager=apt,machine_id=codespaces
 
-    sudo apt update && sudo apt install 1password-cli
-}
-
-function shell() {
-    sudo add-shell "$(which fish)"
-    sudo chsh --shell /home/linuxbrew/.linuxbrew/bin/fish "$USER"
-}
-
-install_1password
-brew bundle install --file dot_homebrew/codespaces.Brewfile
-chezmoi init --apply --source="." --keep-going --force
-
-asdf plugin add python
-asdf plugin add nodejs
-asdf plugin add rust
-asdf install
-
-fish -c "fisher update"
-
-shell
+echo "Running 'chezmoi $*'" >&2
+exec "chezmoi" "$@"
