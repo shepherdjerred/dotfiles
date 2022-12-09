450c2dfdf8372be3d619e564e3ba678b401edc65
Jerred Shepherd <shepherdjerred@gmail.com>
2022-12-09 15:03:39 -0800

    Install chezmoi at HOME

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ set -euv
 if command -v brew >/dev/null; then
     brew install chezmoi
 else
-    sh -c "$(wget -qO- get.chezmoi.io)"
-    export PATH=$PATH:$(pwd)/bin/
+    (cd "$HOME" && sh -c "$(wget -qO- get.chezmoi.io)")
+    export PATH=$PATH:$HOME/bin/
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
