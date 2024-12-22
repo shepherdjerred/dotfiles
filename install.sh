7073a5522f3f19c8e906c6506050413e90cb34fb
Jerred Shepherd <github@sjer.red>
2024-12-22 03:07:27 +0000

    work

--- install.sh
+++ install.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
+export NONINTERACTIVE=1
+
+apt update
+apt upgrade -y
+apt install -y git curl
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /root/.bashrc
+echo 'eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"' >> /root/.bashrc
+eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+
+brew install chezmoi
 chezmoi init --apply https://github.com/shepherdjerred/dotfiles
+
+brew bundle install
+
+# fish
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+echo $HOMEBREW_PREFIX/bin/fish > /etc/shells
+# chsh -s $HOMEBREW_PREFIX/bin/fish
+
+fisher install
